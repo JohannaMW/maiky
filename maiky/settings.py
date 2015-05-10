ea1e02c9ba7af1921105ee27e9627a4fb00c4556
Johanna <jweinsziehr@gmx.de>
2015-05-11 00:52:42 +0200

    amazon

--- maiky/settings.py
+++ maiky/settings.py
@@ -116,9 +116,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-
-
-
 try:
     from local_settings import *
 except ImportError:
--- maiky/wsgi.py
+++ maiky/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lms.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maiky.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
