32e24618974d6ace61e277b2716eaaf4979f2c34
Johanna Weinsziehr <johanna.weinsziehr@gmail.com>
2015-07-08 08:59:49 +0200

    realizing more

--- portfolio/views.py
+++ portfolio/views.py
@@ -21,3 +21,39 @@ def opera(request):
 
 def street_opera(request):
     return render_to_response('street_opera_stockholm.html')
+
+def great_grand_mother(request):
+    return render_to_response('great_grand_mother.html')
+
+def ggm_2(request):
+    return render_to_response('ggm_2.html')
+
+def film(request):
+    return render_to_response('film.html')
+
+def du_fehlst(request):
+    return render_to_response('du_fehlst.html')
+
+def fourfoureight(request):
+    return render_to_response('448.html')
+
+def eighthundret(request):
+    return render_to_response('800.html')
+
+def memorial(request):
+    return render_to_response('memorial.html')
+
+def installation(request):
+    return render_to_response('installation.html')
+
+def reflecting_light(request):
+    return render_to_response('reflecting_light.html')
+
+def inside(request):
+    return render_to_response('inside.html')
+
+def soad(request):
+    return render_to_response('soad.html')
+
+def theatre(request):
+    return render_to_response('theatre.html')
